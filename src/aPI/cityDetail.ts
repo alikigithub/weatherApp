0776016f41fcb0434e418d3f5b9c9017dc872f10
alikigithub <alikia6iwaliid@gmail.com>
2025-01-08 01:07:33 +0500

    final

--- src/aPI/cityDetail.ts
+++ src/aPI/cityDetail.ts
@@ -4,10 +4,10 @@ export const getCityDetail = async (lat: number, lon: number) => {
   try {
     console.log(lat, lon);
     const cityDetail = await axios.get(
-      `https:api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`
     );
     return cityDetail.data;
   } catch (error) {
-    throw new Error("Failed to Fetch City Detail");
+    throw new Error(`Failed to Fetch City Detail  ${error} `);
   }
 };
